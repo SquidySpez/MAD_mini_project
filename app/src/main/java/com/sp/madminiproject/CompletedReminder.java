9deed2b2819e0b97bf1dfe1b81fecb1a55312879
JunCheng <PEHJC05.22@ichat.sp.edu.sg>
2024-01-29 09:20:25 +0800

    Added new activities for the reminder section and dependencies. Started Social fragment which is family section

---
+++ app/src/main/java/com/sp/madminiproject/CompletedReminder.java
@@ -0,0 +1,17 @@
+package com.sp.madminiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletedReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_reminder);
+
+        setTitle("Completed Reminders");
+
+    }
+}
